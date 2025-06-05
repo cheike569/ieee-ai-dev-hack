545cdcd5c99e05628a7f63a8f72c914c97c1ef75
Christoph Heike <ch@pacificcodeline.com>
2025-06-05 11:06:42 -0700

    stage

--- src/components/IntroImage.tsx
+++ src/components/IntroImage.tsx
@@ -3,8 +3,18 @@ import image from '@/images/intro.jpg'
 
 export default function IntroImage() {
   return (
-    <div>
-      <Image src={image} alt={'Intro Image'} width={1280} height={853} className="w-full h-auto" />
+    <div className="relative w-full h-[450px] overflow-hidden">
+      <Image
+        src={image}
+        alt={'Intro Image'}
+        fill
+        className="object-cover object-center w-full h-full"
+        priority
+      />
+      <div className="absolute inset-0
+      opacity-50
+      bg-gradient-to-br from-slate-50 to-slate-300
+      pointer-events-none" />
     </div>
   )
 }
